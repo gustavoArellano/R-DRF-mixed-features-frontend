8d5c99cab14183d78d6a5b86554eb2d5405c5438
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-05-13 23:21:37 -0700

    MyFriendList component updated

---
+++ mixed-features/src/components/MyFriendList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyFriendsList extends Component {
+    state = {}
+
+    render() {
+        return(
+            <div>
+                <h1>My Followers</h1>
+            </div>
+        )
+    }
+}
